07d74736e99af8c9858d40d77261e16b6cc00a49
Camilla Westin <camilla@webstin.se>
2020-12-21 16:31:42 +0100

    Added pages and components

--- src/App.js
+++ src/App.js
@@ -1,1 +1,30 @@
+import React, {Component} from 'react';
+import Nav from './Components/Nav';
+import Home from './Pages/Home'; 
+import About from './Pages/About';
+import Services from './Pages/Services';
+import Blog from './Pages/Blog';
+import Contact from './Pages/Contact';
+import Footer from './Components/Footer/Footer';
 
+class App extends Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return(     
+      <div>
+        <Nav />
+        <Home />
+        <About />
+        <Services />
+        <Blog />
+        <Contact />
+        <Footer />
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ src/Components/Nav.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+class Nav extends Component {
+    render() {
+        return (
+            <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+                <div className="container">
+                    <a className="navbar-brand" href="#">Logo</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarCollapse">
+                        <ul className="navbar-nav mx-auto">
+                            <li className="nav-item active">
+                                <a className="nav-link" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">About</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Services</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Blog</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        )
+    }
+}
+
+export default Nav
