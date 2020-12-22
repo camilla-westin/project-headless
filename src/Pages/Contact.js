7e409ba23c2349026b40c6a9cce767d65c42a811
Camilla Westin <camilla@webstin.se>
2020-12-22 12:10:02 +0100

    Dummy content contact page

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -3,8 +3,11 @@ import React, { Component } from 'react';
 class Contact extends Component {
     render() {
         return(
-            <div>
-                <h1>Contact</h1>
+            <div id="contact">
+                <div className="container">
+                    <h1 className="text-center">Contact</h1>
+                    <p className="intro">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. </p>
+                </div>
             </div>
         )
     }
