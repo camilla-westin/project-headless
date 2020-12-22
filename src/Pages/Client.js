58aa380657def9448c907e892cff8072f9eb3e49
Camilla Westin <camilla@webstin.se>
2020-12-22 13:48:06 +0100

    Connected to contentful

---
+++ src/Pages/Client.js
@@ -0,0 +1,8 @@
+import {createClient} from 'contentful';
+
+const client = contentful.createClient({
+    space: 'a7wcxq2dx0uo',
+    accessToken: 'jWRgdsQUzMBd227EB1l69t7ELY5pNBLAtJta097FgeM'
+})
+
+export default client;
