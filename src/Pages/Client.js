3e40f5b68ef30b2e877cff82d7477d6080f7a2b7
Camilla Westin <camilla@webstin.se>
2021-01-21 09:45:10 +0100

    Removed file

--- src/Pages/Client.js
+++
@@ -1,8 +0,0 @@
-const contentful = require('contentful')
-
-const client = contentful.createClient({
-    space: 'a7wcxq2dx0uo',
-    accessToken: 'jWRgdsQUzMBd227EB1l69t7ELY5pNBLAtJta097FgeM'
-})
-
-export default client;
